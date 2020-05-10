c659563ac5e74a12236e2be4c3ee61f41ce8a18b
joscar <joscar.n@outlook.com>
2020-05-10 16:10:08 +1000

    Rename and fix installed_apps typo

--- paymentsite/settings.py
+++ paymentsite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'entry.apps.EntryConfig'
+    'entryapp.apps.EntryappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- paymentsite/urls.py
+++ paymentsite/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('entry/', include('entry.urls')),
+    path('entryapp/', include('entryapp.urls')),
     path('admin/', admin.site.urls),
 ]
