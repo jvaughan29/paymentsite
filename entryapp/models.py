a60d0d0d701e0bd75caf1600802d1cea005c31c7
joscar <joscar.n@outlook.com>
2020-09-17 20:49:57 +1000

    Bootstrap initial

--- entryapp/models.py
+++ entryapp/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class PaymentEntry(models.Model):
-   name_text = models.CharField(max_length=200)
-   labid_text = models.CharField(max_length=9)
-   amount_text = models.CharField(max_length=200)
-   type_text = models.CharField(max_length=200)
-   location_text = models.CharField(max_length=200)
+   name_text = models.CharField('Patient Name', max_length=200)
+   labid_text = models.CharField('Lab ID', max_length=9)
+   amount_text = models.CharField('Amount', max_length=200)
+   type_text = models.CharField('Type', max_length=200)
+   location_text = models.CharField('Location', max_length=200)
    def __str__(self):
       return self.labid_text
