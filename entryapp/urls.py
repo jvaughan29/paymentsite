697cb5df0c3357448571b45df243d3c128b1158f
joscar <joscar.n@outlook.com>
2020-10-20 11:23:16 +1100

    csv function complete

--- entryapp/urls.py
+++ entryapp/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('results', views.results, name='results'),
+    path('export', views.export, name='export'),
     path('complete', views.complete, name='complete'),
     path('receipt', views.receipt, name='receipt'),
 ]
